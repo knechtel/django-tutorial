4d6507c282963d32459d336e7e68ebf6ff9c2c81
knechtel <maiquelknechtel@gmail.com>
2022-12-20 10:57:54 -0300

    Adicionando url de login.

--- notes/urls.py
+++ notes/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
-
     path('notes/', views.list),
+    path('login-end/', views.login),
+    path('login', views.LoginInterfaceView.as_view()),
 ]
