755f33d7f3faab3aadb728d6e4a442df7d011bdf
knechtel <maiquelknechtel@gmail.com>
2022-12-20 10:59:34 -0300

    Adicionando código para tela de login.

--- notes/views.py
+++ notes/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-
+from django.contrib.auth.views import LoginView
 from .models import Notes
 
+
+class LoginInterfaceView(LoginView):
+    template_name = 'notes/login.html'
 
 def list(request):
     all_notes = Notes.objects.all()
     return render(request, 'notes/notes_list.html', {'notes': all_notes})
+
+
+def login(request):
+    return render(request, 'notes/index.html')
