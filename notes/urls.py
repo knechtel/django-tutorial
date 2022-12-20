b03860f637c0e3989e77bfd5d22465985e5aa531
knechtel <maiquelknechtel@gmail.com>
2022-12-20 15:25:08 -0300

    Ädicionando signup.

--- notes/urls.py
+++ notes/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('login-end/', views.login),
     path('login', views.LoginInterfaceView.as_view()),
     path('logout', views.LogoutInterfaceView.as_view()),
+    path('signup', views.SignupView.as_view(), name='signup'),
 ]
