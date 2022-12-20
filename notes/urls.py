1a26d8403fc238cb7f173a177f9bb28ace2ce51f
knechtel <maiquelknechtel@gmail.com>
2022-12-20 14:14:19 -0300

    Adicionando página de logout.

--- notes/urls.py
+++ notes/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('notes/', views.list),
     path('login-end/', views.login),
     path('login', views.LoginInterfaceView.as_view()),
+    path('logout', views.LogoutInterfaceView.as_view()),
 ]
