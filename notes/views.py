ace0ecdf4077430fcfcb729635cf751a8abeaadf
knechtel <maiquelknechtel@gmail.com>
2022-12-20 14:13:10 -0300

    Adicionando logica de login e logout.

--- notes/views.py
+++ notes/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.decorators import login_required
 from .models import Notes
 
+
+class LogoutInterfaceView(LogoutView):
+    template_name = 'notes/logout.html'
 
 class LoginInterfaceView(LoginView):
     template_name = 'notes/login.html'
 
+
+@login_required
 def list(request):
     all_notes = Notes.objects.all()
     return render(request, 'notes/notes_list.html', {'notes': all_notes})
