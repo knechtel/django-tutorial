7979486ef7721d12d18545097a02b0749e963500
knechtel <maiquelknechtel@gmail.com>
2022-12-20 22:14:37 -0300

    Corrigindo urls.

--- notes/urls.py
+++ notes/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
 
+from django.contrib.auth import views as auth_views
 urlpatterns = [
-    path('notes/', views.list),
+    path('notes/', views.list, name='notes'),
     path('login-end/', views.login),
-    path('login', views.LoginInterfaceView.as_view()),
+    path('login/', views.LoginInterfaceView.as_view(), name='login'),
     path('logout', views.LogoutInterfaceView.as_view()),
     path('signup', views.SignupView.as_view(), name='signup'),
+
 ]
