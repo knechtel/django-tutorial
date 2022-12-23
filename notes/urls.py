beb17e64f3baf2fb0e66e646e45ff8b38016aad1
knechtel <maiquelknechtel@gmail.com>
2022-12-23 00:51:44 -0300

    Adicionando urls.

--- notes/urls.py
+++ notes/urls.py
@@ -4,7 +4,8 @@ from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
-    path('api/v1/', views.NotesList.as_view(), name='test'),
+    path('api/v1/', views.NotesList.as_view(), name='list'),
+    path('api/v1/new', views.NotesCreate.as_view(), name='create'),
     path('notes/', views.list, name='notes'),
     path('login-end/', views.login),
     path('login/', views.LoginInterfaceView.as_view(), name='login'),
