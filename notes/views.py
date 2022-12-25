1a9f8676c9633f0e118b8e90ab5d95b7492e6de2
knechtel <maiquelknechtel@gmail.com>
2022-12-25 14:22:57 -0300

    Adicionando elementos em views.

--- notes/views.py
+++ notes/views.py
@@ -4,15 +4,31 @@ from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
 from django.views.generic.edit import CreateView
 from django.contrib.auth.forms import UserCreationForm
-from rest_framework.generics import ListAPIView
-
+from rest_framework.generics import ListAPIView, CreateAPIView
+from rest_framework.viewsets import ModelViewSet
 from notes.serializers import NotesSerialiazers
 from .models import Notes
+from .serializers import ClientSeriliazers
+
+
+class ClientViewSet(ModelViewSet):
+    serializer_class = ClientSeriliazers
+
+    def create(self, request, *args, **kwargs):
+        equipment = request.equipment
+        _serializer = self.serializer_class(data=request.data,
+                                            context={'equipment': equipment})
+        if _serializer.is_valid():
+            _serializer.save()
+            return Response(data=_serializer.data, status=status.HTTP_201_CREATED)  # NOQA
+        else:
+            return Response(data=_serializer.errors, status=status.HTTP_400_BAD_REQUEST)  # NOQA
 
 
 class NotesList(ListAPIView):
     queryset = Notes.objects.all()
     serializer_class = NotesSerialiazers
+
 
 class SignupView(CreateView):
     form_class = UserCreationForm
@@ -20,13 +36,32 @@ class SignupView(CreateView):
     success_url = '/smart/login'
 
 
+class NotesCreate(CreateAPIView):
+    serializer_class = NotesSerialiazers
+
+    def post(self, request, format=None):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            user_name = serializer.get('user')
+            project_name = serializer.get('project_name')
+            user = Users.objects.get(user=user_name)
+            project = User.objects.get(pk=id)
+            project.user = user
+            project.user.save()
+            return Response(ProjectSerializer(project).data, status=status.HTTP_201_CREATED)
+        else:
+            return HttpResponse("Incorrect Credentials")
+
+
 class RestrictedView(TemplateView):
     template_name = 'notes/restricted.html'
     raise_exception = True  # Raise exception when no access instead of redirect
     permission_denied_message = "You are not allowed here."
 
+
 class LogoutInterfaceView(LogoutView):
     template_name = 'notes/logout.html'
+
 
 class LoginInterfaceView(LoginView):
     template_name = 'notes/login.html'
