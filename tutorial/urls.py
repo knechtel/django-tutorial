7d23f78d80c66ec4075bcc87a5bad5e29f6eaa6e
knechtel <maiquelknechtel@gmail.com>
2022-12-26 09:43:14 -0300

    Formatando código.

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -19,7 +19,6 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
     path('smart/', include('notes.urls')),
